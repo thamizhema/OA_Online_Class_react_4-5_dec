f1faba8d2a1ef8acb4d04ea9e78847e2a9c4e46a
thamizhema <thamizharasan2373@gmail.com>
2024-02-16 17:19:48 +0530

    axios added 16-02-2024

--- my_vite_project/todo/src/Components/Dashboard/Home/Home.jsx
+++ my_vite_project/todo/src/Components/Dashboard/Home/Home.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function Home() {
+  const [users, setUsers] = useState([]);
+
+  async function getUser() {
+    const res = await axios.get("http://localhost:8000/users");
+    setUsers(res.data);
+  }
+
+  function sendMsg() {
+    axios.post("http://localhost:8000/users", { name: "CSS", age: 36 });
+  }
+
   return (
     <div>
       <h1>Home</h1>
+      {users.map((u, i) => {
+        return <div key={i}>{`${u.name}=====${u.age}`}</div>;
+      })}
+      <button onClick={getUser}>Get Users</button>
+      <button onClick={sendMsg}>Post Users</button>
     </div>
   );
 }
